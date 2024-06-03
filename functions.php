2b5aa714eaa1a65da5a4d64aeb8bd2921bf9abba
阿杰 <506907958@qq.com>
2024-06-03 17:18:26 +0800

    修复 评论报warning的问题 修复 php8.3 explode 第二个参数必须是string给了NULL的问题

--- functions.php
+++ functions.php
@@ -1,77 +1,86 @@
 <?php
+
+use Utils\Helper;
+
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-Typecho_Plugin::factory('admin/footer.php')->end = array('Meayair', 'editOptionsTheme');
-Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('Meayair', 'blockquoteExtra');
-Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('Meayair', 'blockquoteExtra');
+Typecho_Plugin::factory('admin/footer.php')->end               = ['Meayair', 'editOptionsTheme'];
+Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = ['Meayair', 'blockquoteExtra'];
+Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = ['Meayair', 'blockquoteExtra'];
+
 class Meayair
 {
     public static function blockquoteExtra($con, $obj, $text)
     {
-        $text = empty($text) ? $con : $text;
+        $text    = empty($text) ? $con : $text;
         $pattern = '/<blockquote class="block-extra block-down"\>(.*?)<\/blockquote>/s';
         preg_match_all($pattern, $text, $matches);
         $text = preg_replace_callback($pattern, 'replaceDownClock', $text);
         return $text;
     }
+
     public static function editOptionsTheme()
     {
-        $currentURL = $_SERVER['REQUEST_URI'];
-        $fileName = basename($currentURL);
-        $fileNameCut = explode('.',$fileName);
-        if($fileNameCut[0] == 'options-theme'){
+        $currentURL  = $_SERVER['REQUEST_URI'];
+        $fileName    = basename($currentURL);
+        $fileNameCut = explode('.', $fileName);
+        if ($fileNameCut[0] == 'options-theme') {
             // todo
-        }else if($fileNameCut[0] == 'write-post' || $fileNameCut[0] == 'write-page'){
-            $css = '<style>#custom-field input,textarea{width:100%;}.wmd-button-row{height:auto;}</style>';echo $css;
+        } else if ($fileNameCut[0] == 'write-post' || $fileNameCut[0] == 'write-page') {
+            $css = '<style>#custom-field input,textarea{width:100%;}.wmd-button-row{height:auto;}</style>';
+            echo $css;
             echo "<script src='" . Helper::options()->themeUrl . '/assets/admin/js/write-post.js' . "'></script>";
         }
     }
 }
 
-function themeConfig($form) {
-    $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点 LOGO 地址'), _t('在这里填入一个图片 URL 地址, 以在网站标题前加上一个 LOGO'));
+function themeConfig($form)
+{
+    $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', null, null, _t('站点 LOGO 地址'), _t('在这里填入一个图片 URL 地址, 以在网站标题前加上一个 LOGO'));
     $form->addInput($logoUrl);
 
-    $logoDisplay = new Typecho_Widget_Helper_Form_Element_Select('logoDisplay', array(
+    $logoDisplay = new Typecho_Widget_Helper_Form_Element_Select('logoDisplay', [
         2 => '仅显示LOGO',
         1 => '仅显示标题',
-        0 => 'logo,标题同时显示'
-    ), 0, _t('logo,标题显示状态'), _t('在这里选择logo,标题显示状态。'));
+        0 => 'logo,标题同时显示',
+    ], 0, _t('logo,标题显示状态'), _t('在这里选择logo,标题显示状态。'));
     $form->addInput($logoDisplay);
 
-    $mainColor = new Typecho_Widget_Helper_Form_Element_Text('mainColor', NULL, NULL, _t('主题色'), _t('填写颜色代码。<a href="https://www.bawge.com/archives/72.html">点击帮忙配色</a>'));
+    $mainColor = new Typecho_Widget_Helper_Form_Element_Text('mainColor', null, null, _t('主题色'), _t('填写颜色代码。<a href="https://www.bawge.com/archives/72.html">点击帮忙配色</a>'));
     $form->addInput($mainColor);
 
-    $backgroundColor = new Typecho_Widget_Helper_Form_Element_Text('backgroundColor', NULL, NULL, _t('背景色'), _t('填写颜色代码。<a href="https://www.bawge.com/archives/72.html">点击帮忙配色</a>'));
+    $backgroundColor = new Typecho_Widget_Helper_Form_Element_Text('backgroundColor', null, null, _t('背景色'), _t('填写颜色代码。<a href="https://www.bawge.com/archives/72.html">点击帮忙配色</a>'));
     $form->addInput($backgroundColor);
 
-    $bannerIds = new Typecho_Widget_Helper_Form_Element_Text('bannerIds', NULL, NULL, _t('轮播设置'), _t('在这里输入文章cid,多个文章请用‘,’分隔'));
+    $bannerIds = new Typecho_Widget_Helper_Form_Element_Text('bannerIds', null, null, _t('轮播设置'), _t('在这里输入文章cid,多个文章请用‘,’分隔'));
     $form->addInput($bannerIds);
 
-    $needAjaxScroll = new Typecho_Widget_Helper_Form_Element_Select('needAjaxScroll', array(
+    $needAjaxScroll = new Typecho_Widget_Helper_Form_Element_Select('needAjaxScroll', [
         1 => '开启',
-        0 => '不开启'
-    ), 0, _t('Ajax-scroll自动加载'), _t('开启后会在浏览到页面底部否自动加载下一页。'));
+        0 => '不开启',
+    ], 0, _t('Ajax-scroll自动加载'), _t('开启后会在浏览到页面底部否自动加载下一页。'));
     $form->addInput($needAjaxScroll);
 
-    $startYear = new Typecho_Widget_Helper_Form_Element_Text('startYear', NULL, NULL, _t('站点起始年份'), _t('在这里填入站点起始年份'));
+    $startYear = new Typecho_Widget_Helper_Form_Element_Text('startYear', null, null, _t('站点起始年份'), _t('在这里填入站点起始年份'));
     $form->addInput($startYear);
-    $statistics = new Typecho_Widget_Helper_Form_Element_Textarea('statistics', NULL, NULL, _t('统计代码'), _t('在这里填入统计代码，将会放在heade内'));
+    $statistics = new Typecho_Widget_Helper_Form_Element_Textarea('statistics', null, null, _t('统计代码'), _t('在这里填入统计代码，将会放在heade内'));
     $form->addInput($statistics);
-    $beianNum = new Typecho_Widget_Helper_Form_Element_Text('beianNum', NULL, NULL, _t('备案号'), _t('在这里填入站点备案号'));
+    $beianNum = new Typecho_Widget_Helper_Form_Element_Text('beianNum', null, null, _t('备案号'), _t('在这里填入站点备案号'));
     $form->addInput($beianNum);
-    $footerExtra = new Typecho_Widget_Helper_Form_Element_Textarea('footerExtra', NULL, NULL, _t('额外footer内容（html）'), _t('如果你有额外想添加到footer的内容，可以添加到这里'));
+    $footerExtra = new Typecho_Widget_Helper_Form_Element_Textarea('footerExtra', null, null, _t('额外footer内容（html）'), _t('如果你有额外想添加到footer的内容，可以添加到这里'));
     $form->addInput($footerExtra);
-    $shuoshuo = new Typecho_Widget_Helper_Form_Element_Text('shuoshuo', NULL, NULL, _t('说说'), _t('这里填写说说的分类号'));
+    $shuoshuo = new Typecho_Widget_Helper_Form_Element_Text('shuoshuo', null, null, _t('说说'), _t('这里填写说说的分类号'));
     $form->addInput($shuoshuo);
 }
 
 
-function themeFields($layout) {
-    $cover = new Typecho_Widget_Helper_Form_Element_Text('cover', NULL, 'w-100', _t('特色图像'), _t('在这里填入一个图片URL地址,用以在文章列表及其他地方'));
+function themeFields($layout)
+{
+    $cover = new Typecho_Widget_Helper_Form_Element_Text('cover', null, 'w-100', _t('特色图像'), _t('在这里填入一个图片URL地址,用以在文章列表及其他地方'));
     $layout->addItem($cover);
 }
 
-function getFirstCharacter($title) {
+function getFirstCharacter($title)
+{
     // 去除字符串中的非字母、非汉字字符
     $title = preg_replace('/[^\p{L}\p{Han}]/u', '', $title);
 
@@ -105,48 +114,51 @@ function getFirstCharacter($title) {
 
 function getArchives($widget)
 {
-  $db = Typecho_Db::get();
-  $rows = $db->fetchAll(
-    $db
-      ->select()
-      ->from("table.contents")
-      ->order("table.contents.created", Typecho_Db::SORT_DESC)
-      ->where("table.contents.type = ?", "post")
-      ->where("table.contents.status = ?", "publish")
-  );
+    $db   = Typecho_Db::get();
+    $rows = $db->fetchAll(
+        $db
+            ->select()
+            ->from("table.contents")
+            ->order("table.contents.created", Typecho_Db::SORT_DESC)
+            ->where("table.contents.type = ?", "post")
+            ->where("table.contents.status = ?", "publish")
+    );
 
-  $stat = [];
-  $midShuoShuo = Helper::options() -> shuoshuo;
-  foreach ($rows as $row) {
-    $row = $widget->filter($row);
-    $isShuoshuo = 0;
-    if(!empty($midShuoShuo)){
-        $querymid = $db->select()->from('table.relationships')
-        ->where('table.relationships.cid = ?', $row["cid"]);
+    $stat        = [];
+    $midShuoShuo = Helper::options()->shuoshuo;
+    foreach ($rows as $row) {
+        $row        = $widget->filter($row);
+        $isShuoshuo = 0;
+        if (!empty($midShuoShuo)) {
+            $querymid = $db->select()->from('table.relationships')
+                           ->where('table.relationships.cid = ?', $row["cid"]);
 
-        $mids = $db->fetchAll($querymid);
-        foreach ($mids as $mid) {
-            if($mid['mid'] == $midShuoShuo){
-                $isShuoshuo = 1;
+            $mids = $db->fetchAll($querymid);
+            foreach ($mids as $mid) {
+                if ($mid['mid'] == $midShuoShuo) {
+                    $isShuoshuo = 1;
+                }
+            }
+            if ($isShuoshuo) {
+                continue;
             }
         }
-        if($isShuoshuo){continue;}
-    }
-    
 
-    $arr = [
-      "cid" => $row["cid"],
-      "title" => $row["title"],
-      "permalink" => $row["permalink"],
-    ];
-    $stat[date("Y", $row["created"])][$row["created"]] = $arr;
-  }
-  return $stat;
+
+        $arr                                               = [
+            "cid"       => $row["cid"],
+            "title"     => $row["title"],
+            "permalink" => $row["permalink"],
+        ];
+        $stat[date("Y", $row["created"])][$row["created"]] = $arr;
+    }
+    return $stat;
 }
 
 
-function theNextPrev($widget){
-    $html = '';
+function theNextPrev($widget)
+{
+    $html       = '';
     $prevResult = getNextPrev(true, $widget);
     $nextResult = getNextPrev(false, $widget);
     if (!$prevResult && !$nextResult) {
@@ -162,71 +174,72 @@ function theNextPrev($widget){
     echo $html;
 }
 
-function getNextPrev($mode, $archive, $fields_name = ''){
+function getNextPrev($mode, $archive, $fields_name = '')
+{
     $options = Helper::options();
-    $db = Typecho_Db::get();
+    $db      = Typecho_Db::get();
     //数据准备
-    $where = null;
+    $where  = null;
     $sorted = null;
     $fields = '';
     //$mode为true查询上文，false查询下文
     if ($mode) {
-        $where = 'table.contents.created < ?';
+        $where  = 'table.contents.created < ?';
         $sorted = Typecho_Db::SORT_DESC;
     } else {
-        $where = 'table.contents.created > ?';
+        $where  = 'table.contents.created > ?';
         $sorted = Typecho_Db::SORT_ASC;
     }
 
-    $mid = $options -> shuoshuo;
-    $query = $db->select()->from('table.contents')
-        ->where($where, $archive->created)
-        ->join('table.relationships', 'table.contents.cid = table.relationships.cid', 'LEFT')
-        ->where('table.relationships.mid <> ? ', $mid)
-        ->where('table.contents.status = ?', 'publish')
-        ->where('table.contents.type = ?', $archive->type)
-        ->where('table.contents.password IS NULL')
-        ->order('table.contents.created', $sorted)
-        ->limit(1);
+    $mid     = $options->shuoshuo;
+    $query   = $db->select()->from('table.contents')
+                  ->where($where, $archive->created)
+                  ->join('table.relationships', 'table.contents.cid = table.relationships.cid', 'LEFT')
+                  ->where('table.relationships.mid <> ? ', $mid)
+                  ->where('table.contents.status = ?', 'publish')
+                  ->where('table.contents.type = ?', $archive->type)
+                  ->where('table.contents.password IS NULL')
+                  ->order('table.contents.created', $sorted)
+                  ->limit(1);
     $content = $db->fetchRow($query);
-    $result = null;
     if ($content) {
-        $content = $archive->filter($content);
-        $title = $content['title'];
+        $content   = $archive->filter($content);
+        $title     = $content['title'];
         $permalink = $content['permalink'];
 
-        if(!empty($fields_name)){
-            $query = $db->select()->from('table.fields')
-            ->where('table.fields.cid = ?', $content['cid'])
-            ->where('table.fields.name = ?', $fields_name)
-            ->limit(1);
+        if (empty($fields_name)) {
+            return compact('fields', 'title', 'permalink');
         }
 
+        $query = $db->select()->from('table.fields')
+                    ->where('table.fields.cid = ?', $content['cid'])
+                    ->where('table.fields.name = ?', $fields_name)
+                    ->limit(1);
+
         $content = $db->fetchRow($query);
-        if ($content) {
+        if ($content and is_array($content) and array_key_exists('str_value', $content)) {
             $fields = $content['str_value'];
         }
 
-        $result = array('fields' => $fields, 'title' => $title, 'permalink' => $permalink);
-    } else {
-        $result = false;
+        return ['fields' => $fields, 'title' => $title, 'permalink' => $permalink];
     }
-    return $result;
+    return false;
 }
 
-function replaceDownClock($matches){
-    $string = $matches[1];
-    $source_name = '？？？';
-    $source_href = '';
-    $source_type = '';
+function replaceDownClock($matches)
+{
+    $string        = $matches[1];
+    $source_name   = '？？？';
+    $source_href   = '';
+    $source_type   = '';
     $source_method = '';
-    $source_code = '';
-    
+    $source_code   = '';
+
     $pattern = '/\[资源名称\]：\[(.*?)\]/s';
     if (preg_match($pattern, $string, $matches)) {
         $source_name = $matches[1];
     }
-    
+
     $pattern = '/\[资源地址\]：\[(.*?)\]/s';
     if (preg_match($pattern, $string, $matches)) {
         $source_href = $matches[1];
@@ -246,27 +259,30 @@ function replaceDownClock($matches){
     if (preg_match($pattern, $string, $matches)) {
         $source_code = $matches[1];
     }
-    if(!empty($source_href)){
-        $html = "<blockquote class=\"block-extra block-down\">
+
+    if (empty($source_href)) {
+        return '';
+    }
+
+    $html              = "<blockquote class=\"block-extra block-down\">
     <div class=\"block-down-title\">
         <p>{$source_name}</p>
         <div class=\"block-down-button\"><p>点击下载</p>{$source_href}</div>
     </div>";
-        $block_down_footer = "";
-        if(!empty($source_type)){
-            $block_down_footer .= "<div class=\"block-down-info\"><span>类型：</span>{$source_type}</div>";
-        }
-        if(!empty($source_method)){
-            $block_down_footer .= "<div class=\"block-down-info\"><span>方式：</span>{$source_method}</div>";
-        }
-        if(!empty($source_code)){
-            $block_down_footer .= "<div class=\"block-down-info\"><span>提取码：</span>{$source_code}</div>";
-        }
-        if(!empty($block_down_footer)){
-            $html .= "<div class=\"block-down-footer\">" . $block_down_footer . "</div>";
-        }
-        $html .= "
-    </blockquote>";
+    $block_down_footer = "";
+    if (!empty($source_type)) {
+        $block_down_footer .= "<div class=\"block-down-info\"><span>类型：</span>{$source_type}</div>";
     }
+    if (!empty($source_method)) {
+        $block_down_footer .= "<div class=\"block-down-info\"><span>方式：</span>{$source_method}</div>";
+    }
+    if (!empty($source_code)) {
+        $block_down_footer .= "<div class=\"block-down-info\"><span>提取码：</span>{$source_code}</div>";
+    }
+    if (!empty($block_down_footer)) {
+        $html .= "<div class=\"block-down-footer\">" . $block_down_footer . "</div>";
+    }
+    $html .= '</blockquote>';
+
     return $html;
 }
